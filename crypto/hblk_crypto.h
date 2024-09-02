d33fbf2a48913def994ce13cf3605d41dd7e6180
FrankOnyemaOrji <f.orji@alustudent.com>
2024-09-02 22:32:11 +0200

    More errors solved

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -18,23 +18,26 @@
 #include <openssl/ssl.h>
 
 
+
 #define EC_CURVE NID_secp256k1
 #define EC_PUB_LEN 65
 #define SIG_MAX_LEN 72
 #define PRI_FILENAME "key.pem"
 #define PUB_FILENAME "key_pub.pem"
 
+
 /**
- * struct sig_s - EC signature structure
- *
- * @sig: Signature buffer.
- * @len: Signature size.
- */
+* struct sig_s - EC Signature structure
+*
+* @sig: Signature buffer.
+* @len: Signature size.
+*/
 typedef struct sig_s
 {
 	uint8_t sig[SIG_MAX_LEN];
 	uint8_t len;
-} sig_t
+} sig_t;
+
 
 uint8_t *sha256(int8_t const *s, size_t len,
 uint8_t digest[SHA256_DIGEST_LENGTH]);
@@ -57,4 +60,3 @@ uint8_t const *msg, size_t msglen, sig_t const *sig);
 /*void _print_hex_buffer(uint8_t const *buf, size_t len);*/
 
 #endif /* HBLK_CRYPTO_H */
-
