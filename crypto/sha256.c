2909e192b8dca071e121309ad1750a1dbd788219
FrankOnyemaOrji <f.orji@alustudent.com>
2024-09-02 22:36:28 +0200

    All errors fixed

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -15,7 +15,6 @@ uint8_t *sha256(int8_t const *s, size_t len,
 uint8_t digest[SHA256_DIGEST_LENGTH])
 {
 	if (!s || !digest)
-		return (NULL)
+		return (NULL);
 	return (SHA256((const unsigned char *)s, len, digest));
 }
-
