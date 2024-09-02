179890fe501aa5788546d924d9a92e8e8580418f
FrankOnyemaOrji <f.orji@alustudent.com>
2024-09-02 22:19:41 +0200

    more error solve

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -32,7 +32,7 @@
  */
 typedef struct sig_s
 {
-	unint8_t sig[SIG_MAX_LEN];
+	uint8_t sig[SIG_MAX_LEN];
 	uint8_t len;
 } sig_t
 
@@ -40,7 +40,7 @@ uint8_t *sha256(int8_t const *s, size_t len,
 uint8_t digest[SHA256_DIGEST_LENGTH]);
 
 EC_KEY *ec_create(void);
-uint8_t *ec_to_pub(EC_KEY const *ky, uint8_t pub[EC_PUB_LEN]);
+uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
 
@@ -51,6 +51,8 @@ EC_KEY *ec_load(char const *folder);
 uint8_t *ec_sign(EC_KEY const *key,
 uint8_t const *msg, size_t msglen, sig_t const *sig);
 
+int ec_verify(EC_KEY const *key,
+uint8_t const *msg, size_t msglen, sig_t const *sig);
 /* void _print_hex_buffer(uint8_t const *buf, size_t len);*/
 #endif /* HBLK_CRYPTO_H */
 
