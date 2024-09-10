51045f81df37b9dc58af16aefa8607f148041397
FrankOnyemaOrji <f.orji@alustudent.com>
2024-09-10 14:24:24 +0200

    new changes to solve error

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -9,7 +9,7 @@
  * Return: pointer to the hash_buf
  * Author: Frank Onyema Orji
  */
-unint8_t *block_hash(block_t const *block,
+uint8_t *block_hash(block_t const *block,
 		uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
 	size_t len = sizeof(block->info) + block->data.len;
