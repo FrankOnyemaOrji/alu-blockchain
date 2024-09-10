2585c682cd99936cfb4d95fe30d9fb92bcb8fd54
FrankOnyemaOrji <f.orji@alustudent.com>
2024-09-10 13:45:04 +0200

    Task one and two solved

---
+++ blockchain/block_destroy.c
@@ -0,0 +1,13 @@
+#include "blockchain"
+
+/**
+ * block_destroy - deletes an exisiting block
+ *
+ * @block: pointer to the block to delete
+ * Author: Frank Onyema Orji
+ */
+void block_destroy(block_t *block)
+{
+	free(block);
+}
+
