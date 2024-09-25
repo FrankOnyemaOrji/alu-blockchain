183c602c5b1da08c31a0ae074f7e886f67bd96ed
FrankOnyemaOrji <f.orji@alustudent.com>
2024-09-25 10:13:51 +0200

    Blockchain v0.2 error corrected in block_mine

--- blockchain/v0.2/block_mine.c
+++ blockchain/v0.2/block_mine.c
@@ -7,24 +7,12 @@
  */
 void block_mine(block_t *block)
 {
-	/* nonce is a 64-bit number that will be used once */
 	uint64_t nonce = 0;
 
-	uint8_t hash[SHA256_DIGEST_LENGTH];
-
-	while (1)
-	{
-		/* Update the block's info with the new nonce */
+	do {
 		block->info.nonce = nonce;
-
-		block_hash(block, hash);
-
-		if (hash_matches_difficulty(hash, block->info.difficulty))
-			break;
-
+		block_hash(block, block->hash);
 		nonce++;
-	}
-	/* Copy the resulting hash to the block's data */
-	memcpy(block->hash, hash, SHA256_DIGEST_LENGTH);
+	} while (!hash_matches_difficulty(block->hash, block->info.difficulty));
 }
 
